1c9f918b144835819c60a5ce94d0b8fa53bbb241
Shivanshi Nigam <130684919+shivanshinigam@users.noreply.github.com>
2025-06-11 21:45:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aievaluationsuite.netlify.app/","http://localhost:3000" ],
+    allow_origins=["https://aievaluationsuite.netlify.app","http://localhost:3000" ],
     
     allow_credentials=True,
     allow_methods=["*"],
